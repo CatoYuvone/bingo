79c96989afc82e4e3d776685ab649a5409088947
Cato <otacyuvo@gmail.com>
2020-05-20 15:52:44 -0300

    Nuevo test que valida que que en cada columna los números de las celdas asciendan de arriba a abajo

--- src/bingo.py
+++ src/bingo.py
@@ -25,7 +25,7 @@ def validar_quince_numeros(carton):
 def avanzar_de_izquierda_a_derecha(carton):
     bandera = 1
     for fila in carton:                           # Recorro el carton por filas
-       celda_aux = 0                              # Igualo celda_aux a 0       
+        celda_aux = 0                             # Igualo celda_aux a 0       
         for celda in fila:                        # Recorro la fila por celdas
             if celda != 0:                        # Si la celda no está vacía
                if celda <= celda_aux:             # Comparo celda con celda_aux
@@ -34,4 +34,19 @@ def avanzar_de_izquierda_a_derecha(carton):
 
     return bandera                                # Retorno 1 si en ninguna fila hay una celda con un número menor o igual al número de la celda de su izquierda, 0 en caso contrario
  
+
+
+def avanzar_de_arriba_a_abajo(carton):
+    fila = 0
+    columna = 0
+    bandera = 1
+    for fila in range(0, 2):
+        for columna in range(0, 9):
+            if carton[fila][columna] >= carton[fila+1][columna]  and carton[fila][columna] != 0 and carton[fila+1][columna] != 0:        # Si la celda de arriba es mayor o igual que la de abajo y las dos están ocupadas 
+               bandera = 0                                              # Entonces bandera = 0
+            columna = columna + 1                                       # Súmole uno al contador de columnas
+        fila = fila + 1                                                 # Súmole  al contador de filas
+    
+    return bandera   # Retorno 0 si alguna celda no es menor que la celda de debajo suya, 1 en caso contrario                  
              
+
