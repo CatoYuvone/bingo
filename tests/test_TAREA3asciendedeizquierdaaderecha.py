c3058264946af7d9952776a0594bb188e1effcfc
Cato <otacyuvo@gmail.com>
2020-06-29 10:37:55 -0300

    Hago que el test TAREA3asciendedeizquierdaaderecha use el cartón de bingo.py

--- tests/test_TAREA3asciendedeizquierdaaderecha.py
+++ tests/test_TAREA3asciendedeizquierdaaderecha.py
@@ -2,10 +2,6 @@ from src.bingo import carton
 from src.bingo import avanzar_de_izquierda_a_derecha
 
 def test_ascendiente():
-    carton =  (
-        (1, 2, 3, 4, 0, 5, 6, 7, 8),
-        (1, 5, 6, 0, 7, 0, 8, 0, 9), 
-        (3, 0, 5, 14, 16, 67, 0, 0, 0)
-      )
-    assert avanzar_de_izquierda_a_derecha(carton) == 1
+    micarton = carton()
+    assert avanzar_de_izquierda_a_derecha(micarton) == 1
     # Afirmo que los números de la fila asciendan de izquierda a derecha
