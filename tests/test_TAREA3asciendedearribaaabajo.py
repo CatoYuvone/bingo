507ad800e635e529ab045027158a4e505a9e6ce9
Cato <otacyuvo@gmail.com>
2020-06-29 10:34:54 -0300

    Para el test TAREA3asciendedearribaaabajo uso el carton de bingo.py

--- tests/test_TAREA3asciendedearribaaabajo.py
+++ tests/test_TAREA3asciendedearribaaabajo.py
@@ -2,10 +2,6 @@ from src.bingo import carton
 from src.bingo import avanzar_de_arriba_a_abajo
 
 def test_ascendiente():
-    carton =  (
-        (1, 2, 3, 4, 0, 5, 6, 7, 8),
-        (2, 5, 6, 0, 7, 0, 8, 0, 9), 
-        (3, 0, 8, 14, 16, 67, 0, 0, 0)
-      )
-    assert avanzar_de_arriba_a_abajo(carton) == 1
-    # Afirmo que los números de la columna asciendden de arriba a abajo
+    micarton = carton() 
+    assert avanzar_de_arriba_a_abajo(micarton) == 1
+    # Afirmo que los números de la columna ascienden de arriba a abajo
