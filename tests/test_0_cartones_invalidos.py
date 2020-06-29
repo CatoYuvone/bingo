5b714d41bdc00b6a99c14041095d80904051acbe
Cato <otacyuvo@gmail.com>
2020-06-29 20:07:14 -0300

    Agrego el test de avnazar a izquierda a derecha al de cartones inválidos

--- tests/test_0_cartones_invalidos.py
+++ tests/test_0_cartones_invalidos.py
@@ -1,7 +1,7 @@
 from src import bingo
 
 micarton = (
-      (0, 0, 0, 6, 0, 0, 0, 0, 0),
+      (0, 0, 0, 6, 0, 0, 0, 2, 0),
       (0, 0, 0, 4, 0, 0, 0, 0, 0),
       (0, 0, 0, 0, 0, 0, 0, 0, 0),
       (0, 0, 0, 0, 0, 43, 0, 0, 0)
@@ -10,3 +10,6 @@ micarton = (
 
 def test_15_celdas():
     assert not bingo.validar_quince_numeros(micarton)
+
+def test_izq_a_der():
+    assert bingo.avanzar_de_izquierda_a_derecha(micarton) != 1
