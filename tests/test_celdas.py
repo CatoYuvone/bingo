f54bb58cf9444e3033cb8b87ac0d8c62baff6b1e
Cato <otacyuvo@gmail.com>
2020-06-29 10:28:02 -0300

    Para el test_celdas usar el carton de bingo.py

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -2,15 +2,11 @@ from src.bingo import carton
 from src.bingo import validar_quince_numeros
 
 def test_contar_celdas_ocupadas():
-    carton =  (
-       (1, 0, 1, 1, 0, 0, 0, 0, 1),
-       (1, 1, 1, 0, 1, 0, 1, 1, 1),
-       (1, 0, 0, 1, 0, 1, 0, 1, 0)
-    )
+    micarton = carton()
     contador = 0
-    for fila in carton:
+    for fila in micarton:
         for celda in fila:
             contador = contador + celda
 
     #Esperamos encontrar 15 celdas ocupadas.
-    assert validar_quince_numeros(carton) == True
+    assert validar_quince_numeros(micarton) == True
